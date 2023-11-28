99f605307de5b7118e09b7e7bd6ffec1efd1268e
William-Dores <williamdores.s132@outlook.com>
2023-11-28 00:36:30 -0300

    Service layer, component registration

--- src/main/java/com/treinandospring/course/repositories/UserRepository.java
+++ src/main/java/com/treinandospring/course/repositories/UserRepository.java
@@ -1,13 +1,8 @@
 package com.treinandospring.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.treinandospring.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	
-	
-	
-	
-
 }
