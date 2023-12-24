ddf6efcb58e0d084df69b80d2730952748cd71cc
William-Dores <williamdores.s132@outlook.com>
2023-12-24 11:02:29 -0300

    Entitie Payment and association with Order one-to-one

--- src/main/java/com/treinandospring/course/config/TestConfig.java
+++ src/main/java/com/treinandospring/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.treinandospring.course.entities.Category;
 import com.treinandospring.course.entities.Order;
 import com.treinandospring.course.entities.OrderItem;
+import com.treinandospring.course.entities.Payment;
 import com.treinandospring.course.entities.Product;
 import com.treinandospring.course.entities.User;
 import com.treinandospring.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
